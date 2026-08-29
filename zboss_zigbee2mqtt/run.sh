1bb754033e0c7132998173b14b4b930df18e6624
wesioplayconstructor <wesio@wesioiot.top>
2026-08-29 20:47:26 +0000

    fix: change shebang from /usr/bin/env bash to /bin/sh (v2.13.0-9)

--- zboss_zigbee2mqtt/run.sh
+++ zboss_zigbee2mqtt/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 if [ -f /data/options.json ]; then
     # Substitui a pasta antiga /config/zigbee2mqtt por /config/zboss_zigbee2mqtt no options.json
     sed -i 's|"/config/zigbee2mqtt"|"/config/zboss_zigbee2mqtt"|g' /data/options.json
